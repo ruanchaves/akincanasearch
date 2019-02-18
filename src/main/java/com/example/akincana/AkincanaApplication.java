6f301d9cccd3ad0ea6a03a84e6df21d7be628410
ruanchaves93 <ruanchaves93@gmail.com>
2019-02-18 19:05:33 -0300

    Spring Initializr

---
+++ src/main/java/com/example/akincana/AkincanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.akincana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkincanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkincanaApplication.class, args);
+	}
+
+}
