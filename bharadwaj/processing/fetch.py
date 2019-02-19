f4553cf2670c3e06399bfebee5aca07454828e52
ruanchaves93 <ruanchaves93@gmail.com>
2019-02-19 18:54:06 -0300

    Added image cropping and structured pipeline for Bharadwaj Samhita preprocessing step

--- bharadwaj/processing/fetch.py
+++ bharadwaj/processing/fetch.py
@@ -6,22 +6,14 @@ import re
 import requests
 import yaml
 import io
-from PIL import Image, ImageChops
 from cltk.corpus.sanskrit.itrans.unicode_transliterate import ItransTransliterator as itt
+from PIL import Image, ImageChops, ImageOps
+from itertools import accumulate
 
 CONFIG_FILE = "config.yml"
 config = yaml.load(open(CONFIG_FILE, "r"))
 logger.add(config['logfile'])
 
-def trim(im):
-    # fraxel https://stackoverflow.com/users/1175101/fraxel
-    # https://stackoverflow.com/a/10616717/4882300
-    bg = Image.new(im.mode, im.size, im.getpixel((0,0)) )
-    diff = ImageChops.difference(im, bg)
-    diff = ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
-    if bbox:
-        return im.crop(bbox)
 
 def safe_run(function, *args):
     try:
@@ -52,36 +44,129 @@ class Fetcher(object):
                                      ) \
                           ) for x in self.links ]
 
-def split_verses(text):
-    data = []
-    pattern = re.compile("(?<=[(॥)?])\s*")
-    for page in text:
-        inv_page = page[::-1]
-        matches = pattern.finditer(inv_page)
-        bar_positions = [ match.start() - 1 for match in matches ]
-        assert len(bar_positions) % 2 == 0
-        vpos = [ v for i,v in enumerate(bar_positions) if not i % 2 ]
-        vpos = [ (vpos[i],vpos[i+1]-1) for i in range(len(vpos) - 1) ]
-        last = list(vpos.pop())
-        last[1] = len(inv_page)
-        vpos.append(tuple(last))
-        verses = [ inv_page[tup[0]:tup[1]][::-1] for tup in vpos ][::-1]
-        data.append(verses)
-    return data
+class Parser(object):
+
+    def __init__(self, pages):
+        self.pages = pages
+        self.text = []
+        self.roman = []
+        self.window = []
+
+    def split(self):
+        data = []
+        pattern = re.compile("(?<=[(॥)?])\s*")
+        for page in self.pages:
+            inv_page = page[::-1]
+            matches = pattern.finditer(inv_page)
+            bar_positions = [ match.start() - 1 for match in matches ]
+            assert len(bar_positions) % 2 == 0
+            vpos = [ v for i,v in enumerate(bar_positions) if not i % 2 ]
+            vpos = [ (vpos[i],vpos[i+1]-1) for i in range(len(vpos) - 1) ]
+            last = list(vpos.pop())
+            last[1] = len(inv_page)
+            vpos.append(tuple(last))
+            verses = [ inv_page[tup[0]:tup[1]][::-1] for tup in vpos ][::-1]
+            data.append(verses)
+        self.text = data
+
+    def extend(self, tup, offset):
+        left = tup[0]
+        right = tup[1]
+        diff = right - left
+        plus = offset * diff
+        new_left = left - plus
+        new_right = right + plus
+        if new_left < 0.0:
+            new_left = 0.0
+        if new_right > 1.0:
+            new_right = 1.0
+        return (new_left, new_right)
+
+    def make_window(self, offset=0.5):
+        self.window = []
+        for page in self.text:
+            sizes = list(accumulate([len(verse) for verse in page]))
+            sizes = [ x/sizes[-1] for x in sizes ]
+            sizes = [0.0] + [ x for y in list(zip(sizes,sizes)) for x in y ]
+            sizes = [(sizes[i], sizes[i+1]) for i in range(0,len(sizes) - 1, 2)]
+            sizes = [ self.extend(tup, offset) for tup in sizes ]
+            self.window.append(sizes)
+
+    def romanize(self):
+        self.roman = [[itt.to_itrans(x, config['itrans_language']) for x in y] for y in self.text]
+
+class Transform(Parser, Fetcher):
+
+    def __init__(self, sample=None, images=None, pages=None):
+        self.images = images
+        self.pages = pages
+        self.sample = sample
+        self.noborder = []
+        self.cropped = []
+        if not images:
+            try:
+                super().fetch_links()
+            except Exception as e:
+                logger.debug(e)
+
+    def build(self):
+        if self.sample:
+            selected_links = [ self.links[i] for i in self.sample ]
+            self.links = selected_links
+        if not self.images:
+            super().fetch_images()
+        if not self.pages:
+            pages = [ pytesseract.image_to_string( \
+                                                  x, \
+                                                  lang=config['tesseract_language'], \
+                                                  config=config['tesseract_parameters'] \
+                                                  ) \
+                     for x in self.images ]
+        super().__init__(pages=pages)
+
+    def crop_border(self, im):
+        # fraxel https://stackoverflow.com/users/1175101/fraxel
+        # https://stackoverflow.com/a/10616717/4882300
+        bg = Image.new(im.mode, im.size, im.getpixel((0,0)) )
+        diff = ImageChops.difference(im, bg)
+        diff = ImageChops.add(diff, diff, 2.0, -100)
+        bbox = diff.getbbox()
+        if bbox:
+            return im.crop(bbox)
+
+    def crop_all_border(self):
+        self.noborder = [ self.crop_border(x) for x in self.images ]
+
+    def crop(self):
+        super().split()
+        super().make_window()
+        window = self.window
+        assert len(self.images) == len(self.window)
+        self.crop_all_border()
+        self.cropped = [ self.process(self.noborder[i], v ) for i,v in enumerate(self.window) ]
+
+    def process(self, image, labels):
+        images = []
+        for tup in labels:
+            begin = tup[0]
+            end = 1.0 - tup[1]
+            width, height = image.size
+            begin *= height
+            end *= height
+            begin = int(begin)
+            end = int(end)
+            border = (0, begin, 0, end)
+            result = ImageOps.crop(image, border)
+            images.append(result)
+        return images
 
 if __name__ == '__main__':
-    f = Fetcher()
-    f.fetch_links()
-    f.links = [f.links[50]]
-    links = f.links
-    print(links)
-    f.fetch_images()
-    images = f.images
     # TO-DO ! make dir if not exists
     safe_run(os.chdir, "../../datasets/bharadwaj/scans")
-    trimmed_images = [ trim(x) for x in images ]
-    #trimmed_images[0].show()
-    pages = [ pytesseract.image_to_string(x, lang=config['tesseract_language'], \
-                                         config=config['tesseract_parameters']) for x in images ]
-    verses = split_verses(pages)
-    roman_verses = [[itt.to_itrans(x, config['itrans_language']) for x in y] for y in verses]
+    i = Transform(sample=[50])
+    i.build()
+    i.crop()
+    i.cropped[0][0].show()
+    i.cropped[0][1].show()
+    i.cropped[0][2].show()
+    i.cropped[0][3].show()
